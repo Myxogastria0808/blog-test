a6256dd0ee93a1ffb373dbba88ec24111dbcb48a
Myxogastria0808 <r.rstudio.c@gmail.com>
2023-01-28 17:53:01 +0900

    Hello

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     body = db.Column(db.String(300), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(), onupdate=datetime.now())
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
